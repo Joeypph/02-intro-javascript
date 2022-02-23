3817cd6482ccad7c378d6a24d241c07ffb0a8ffc
Joeypph <joseant_1098@hotmail.com>
2022-02-23 10:53:12 -0600

    exporting functions to another file

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -2,13 +2,14 @@ import heroes,{owners} from "../data/heroes";
 
 // import { heroes } from "./data/heroes";
 
-console.log('Los owners',owners);
-const getHeroeById = (id) => {
+export const getHeroeById = (id) => {
     return heroes.find(hero => hero.id === id);
 }
-const getHeroeByOwner = (owner) => {
+export const getHeroeByOwner = (owner) => {
     return heroes.filter(hero => hero.owner === owner);
 }
-console.log(getHeroeById(2));
-console.log(getHeroeByOwner('DC'));
+/* console.log(getHeroeById(2));
+console.log(getHeroeByOwner('DC')); */
 
+
+// export default getHeroeById;
